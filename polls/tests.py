3cc21be5987f0d3d9b1090ddb0eb2ef9c1163a3b
Alex Markowitz <admarko@gmail.com>
2019-12-11 21:51:20 -0500

    finished part 5 - tests

--- polls/tests.py
+++ polls/tests.py
@@ -36,12 +36,12 @@ class QuestionModelTests(TestCase):
         self.assertIs(recent_question.was_published_recently(), True)
 
 
-def create_question(question_text: str, days: int):
+def create_question(question_text: str, days: int) -> Question:
     """
-    create q with text and offset days (negative for past)
+    helper to create a Question with text and offset days (negative for past)
     """
     time = timezone.now() + datetime.timedelta(days=days)
-    Question.objects.create(question_text=question_text, pub_date=time)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionIndexViewTests(TestCase):
@@ -97,3 +97,25 @@ class QuestionIndexViewTests(TestCase):
             response.context["latest_question_list"],
             ["<Question: Past question 2.>", "<Question: Past question 1.>"],
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
